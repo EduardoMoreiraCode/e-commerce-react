269220b7f284acb8215c3f89e3f9b12e6a88eed2
EduardoMoreiraCode <eduu.eduardo09@gmail.com>
2024-02-03 21:17:51 -0300

    fix: menu alignment

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import menuIcon from "../../assets/icons/menu-icon.png";
+import closeMenu from "../../assets/icons/when-menu-is-open-icon.png";
+import Menu from '../Menu/Menu';
 const Header = () => {
+    const [isMenuIconClicked, setMenuIconClicked] = useState(false);
+
+    const trocarIcon = () => {
+        // para inverter o estado, inverto o valor da prev
+        setMenuIconClicked((estado) => {
+            return !estado;
+        });
+      };
+
+    const fecharMenu = () => {
+        setMenuIconClicked(false);
+    };
+
+    console.log(setMenuIconClicked)
+    
     return (
-        <header className='header'>
+        <div>
+            <header className='header'>
             <div className='header-title'>ShoeStore</div>
             <div className='header-menu-icon'>
-                <img src={menuIcon} alt='menu-icon'/>
+                <img onClick={trocarIcon} id='menu-icon' src={isMenuIconClicked ? closeMenu : menuIcon} alt='menu-icon'/>
             </div>
         </header>
+        <Menu isVisible={isMenuIconClicked} onClose={fecharMenu} />
+        </div>
     )
+
 }
 
 export default Header
