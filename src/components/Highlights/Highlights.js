55a3eb802cfc020a75a4520736d9aad9b07e1167
EduardoMoreiraCode <eduu.eduardo09@gmail.com>
2025-01-18 02:34:16 -0300

    feat: media queries and wrap

--- src/components/Highlights/Highlights.js
+++ src/components/Highlights/Highlights.js
@@ -17,7 +17,8 @@ const Highlights = () => {
       </div>
 <div className='highs'>
 
-<div className='high-item1'>
+<div className='high-item'> 
+  {/* high-item1 */}
   <Card>
     <div className='card-content'>
       <img className='high-image' src={nikeOne} alt='Nike One'/>
@@ -72,6 +73,56 @@ const Highlights = () => {
     <div className='card-content'>
       <img className='high-image' src={casualSport} alt='Nike Beige Sport Casual'/>
       <span title='Nike Beige Sport Casual' className='card-text'>Nike Beige Sports</span>
+    </div>
+    <Button>Compre já</Button>
+  </Card>
+</div>
+
+<div className='high-item'>
+  <Card>
+    <div className='card-content'>
+      <img className='high-image' src={nikeOne} alt='Nike One'/>
+      <span title='Nike Suns 2TS' className='card-text'>Nike Suns 2TS</span>
+    </div>
+    <Button>Compre já</Button>
+  </Card>
+</div>
+
+<div className='high-item'>
+  <Card>
+    <div className='card-content'>
+      <img className='high-image' src={nike2} alt='Nike Air Jordan White 2XS'/>
+      <span title='Nike Air Jordan White 2XS' className='card-text'>Nike Air Jordan White 2XS</span>
+    </div>
+    <Button>Compre já</Button>
+  </Card>
+</div>
+
+<div className='high-item'>
+  <Card>
+    <div className='card-content'>
+      <img className='high-image' src={basketShoe} alt='Nike Super Pro Max Omega'/>
+      <span title='Nike Super Pro Max Omega' className='card-text'>Nike Super Pro Max Omega</span>
+    </div>
+    <Button>Compre já</Button>
+  </Card>
+</div>
+
+<div className='high-item'>
+  <Card>
+    <div className='card-content'>
+      <img className='high-image' src={adidasGreen} alt='Adidas Green-White SportsRace'/>
+      <span title='Adidas Green-White SportsRace' className='card-text'>Adidas Green-White SportsRace</span>
+    </div>
+    <Button>Compre já</Button>
+  </Card>
+</div>
+
+<div className='high-item'>
+  <Card>
+    <div className='card-content'>
+      <img className='high-image' src={orangeNike} alt='Nike Casual-Social Orange-White'/>
+      <span title='Nike Casual-Social Orange-White' className='card-text'>Nike Casual-Social Orange-White</span>
     </div>
     <Button>Compre já</Button>
   </Card>
